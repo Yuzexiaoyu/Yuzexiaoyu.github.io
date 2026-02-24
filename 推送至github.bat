ac341d96fa2fc906813d2a5417c1ed908b6b75e4
Yuzexiaoyu <2045757235@qq.com>
2026-02-24 17:24:32 +0800

    CDN基建完成

---
+++ 推送至github.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001 >nul
+cd /d F:\Yuzexiaoyu.space
+
+git add .
+
+set /p MSG="提交消息: "
+if "%MSG%"=="" set MSG=Update site content
+
+git commit -m "%MSG%" >nul 2>&1
+git push
+
+if errorlevel 1 (
+    echo.
+    echo 推送失败
+) else (
+    echo.
+    echo 推送成功！
+    echo 网站将在 2 分钟内更新
+    echo https://yuzexiaoyu.space
+)
+
+pause
